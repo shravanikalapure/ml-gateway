01cd7068ec18b542e6de0ddd55b361ff2ea665d7
Shravani <kalapureshravani@gmail.com>
2026-09-06 15:41:08 +0530

    fixed errors

--- generated/inference_pb2_grpc.py
+++ generated/inference_pb2_grpc.py
@@ -3,12 +3,9 @@
 import grpc
 import warnings
 
-try:
-    from . import inference_pb2 as inference__pb2
-except ImportError:
-    import inference_pb2 as inference__pb2
+from . import inference_pb2 as inference__pb2
 
-GRPC_GENERATED_VERSION = '1.83.1'
+GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
@@ -28,7 +25,7 @@ if _version_not_supported:
     )
 
 
-class InferenceServiceStub:
+class InferenceServiceStub(object):
     """Missing associated documentation comment in .proto file."""
 
     def __init__(self, channel):
@@ -49,7 +46,7 @@ class InferenceServiceStub:
                 _registered_method=True)
 
 
-class InferenceServiceServicer:
+class InferenceServiceServicer(object):
     """Missing associated documentation comment in .proto file."""
 
     def Predict(self, request, context):
@@ -85,7 +82,7 @@ def add_InferenceServiceServicer_to_server(servicer, server):
 
 
  # This class is part of an EXPERIMENTAL API.
-class InferenceService:
+class InferenceService(object):
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
